2a5a3d0791d01a3570fdfa9d4fafd34ca30591a3
joyshaheb <khondokoralam@gmail.com>
2023-09-12 21:54:06 +0600

    layout files added

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
